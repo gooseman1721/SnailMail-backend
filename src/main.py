a027edab094e0255bff2459aeaefc975bc02eba1
Gooseman <theados123@gmail.com>
2022-10-02 19:55:48 +0200

    Message POST endpoint now checks: if the provided sender_id is authenticated user's id and if the provided receiver_id is a valid user_id

--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
 from datetime import datetime, timedelta
-from msilib import schema
-from tkinter import E
 
 from fastapi import Depends, FastAPI, HTTPException, status, Request
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -83,7 +81,6 @@ def get_user_received_messages(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db=db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    print("pleple")
     return crud.get_user_received_messages(db=db, user_id=user_id)
 
 
@@ -101,8 +98,19 @@ def create_message(
     sender_id: int,
     receiver_id: int,
     db: Session = Depends(get_db),
-    # token: str = Depends(oauth2_scheme),
+    token: str = Depends(oauth2_scheme),
 ):
+    token_data = authenticate_user(token=token, db=db)
+    user_id = crud.convert_user_name_to_user_id(db=db, user_name=token_data.username)
+
+    if user_id != sender_id:
+        raise HTTPException(status_code=401, detail="Unauthorized sender")
+
+    db_receiver = crud.get_user(db=db, user_id=receiver_id)
+
+    if db_receiver is None:
+        raise HTTPException(status_code=404, detail="Receiver not found")
+
     return crud.create_message(
         db=db, message=message, sender_id=sender_id, receiver_id=receiver_id
     )
